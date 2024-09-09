221f4d08ae7bbd8074b98cb620e5314f5dd25b17
RiccardoMacchi <rick.mac97@outlook.com>
2024-09-09 17:53:38 +0200

    created menu nav add function for rand numbers in array

--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,28 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+// funzione per generazione numberi random da stampare in home
+function genNum(){
+    $numbers = [];
+    for($i = 0; $i < 25; $i++){
+        $number = rand(1,100);
+        if(!in_array($number,$numbers)){
+            $numbers[] = $number;
+        }
+    }
+    return $numbers;
+}
 
 Route::get('/', function () {
     $title = 'Hello Laravel';
-    $numbers = [2,3,7,19,35,38,49,50];
+    $numbers = genNum();
 
     return view('home', compact('title','numbers'));
+});
+
+Route::get('/chi-siamo', function (){
+    $title = 'Chi siamo?';
+    $description = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?';
+
+    return view('about', compact('title','description'));
 });
